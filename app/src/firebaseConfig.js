0c7e73f29c80273ed0420b159659d12767de78fd
anishs <anish2sinha@gmail.com>
2024-09-12 11:59:18 -0400

    added .env file

--- app/src/firebaseConfig.js
+++ app/src/firebaseConfig.js
@@ -7,12 +7,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCxsXcoGFTYOna8DhIdxpo4DpQ1-Q7aBX8",
-    authDomain: "ec463-mini-app.firebaseapp.com",
-    projectId: "ec463-mini-app",
-    storageBucket: "ec463-mini-app.appspot.com",
-    messagingSenderId: "775318854082",
-    appId: "1:775318854082:web:6dc195feeecf7c783dde5b"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
