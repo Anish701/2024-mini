26c622e6c4de8e79b6e2c0b89b4319764331ba7d
anishs <anish2sinha@gmail.com>
2024-09-13 17:38:29 -0400

    added input box to post notes onto cloud

--- app/src/Notes.js
+++ app/src/Notes.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { auth } from './firebaseConfig';
+import { auth, db } from './firebaseConfig';
+import { collection, addDoc } from 'firebase/firestore';
 
 const Notes = () => {
     const [userName, setUserName] = useState('');
+    const [newNote, setNewNote] = useState("")
 
     useEffect(() => {
         if (auth.currentUser) {
@@ -10,9 +12,26 @@ const Notes = () => {
         }
     }, []);
 
+    const handleKeyDown = async (e) => {
+        if (e.key === 'Enter') {
+            await addDoc(collection(db, 'notes'), {
+                uid: auth.currentUser.uid,
+                note: e.target.value,
+                createdAt: new Date(),
+            });
+            setNewNote(e.target.value);
+            e.target.value = '';
+        }
+    };
+
     return (
         <div className="Notes" style={{ textAlign: 'center', marginTop: '100px' }}>
             <h1>Here are your notes, {userName}</h1>
+            <input
+                placeholder="Write New Note"
+                type="text"
+                onKeyDown={handleKeyDown}
+            />
         </div>
     );
 };
